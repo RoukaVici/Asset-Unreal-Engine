5bbfe0c83ffe9b0c789bb32868d4d6e5933795f1
Louis Jahn <louis.jahn@epitech.eu>
2018-06-12 16:41:23 +0200

    Fixed folder hierarchy, git will be much lighter

--- POC_Unreal/Intermediate/Build/Win64/RoukaVici/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Paul/Documents/Rendu/POC_Unreal_Engine/trunk/POC_Unreal/Intermediate/Build/Win64/RoukaVici/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- POC_Unreal/Intermediate/Build/Win64/RoukaVici/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for C:\Users\Paul\Documents\Rendu\POC_Unreal_Engine\trunk\POC_Unreal\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "C:\Users\Paul\Documents\Rendu\POC_Unreal_Engine\trunk\POC_Unreal\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
